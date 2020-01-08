25ca57757ff2903073af799a43f75ed7df1ecd5b
AntonAroche <anton.aroche@gmail.com>
2020-01-08 10:35:46 -0500

    Added feature to add songs to a band's songlist

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -1,12 +1,12 @@
 import Controller from "@ember/controller";
-import Band from 'rarwe/models/band';
-import { empty } from '@ember/object/computed';
+import Band from "rarwe/models/band";
+import { empty } from "@ember/object/computed";
 
 export default Controller.extend({
   isAddingBand: false,
-  newBandName: '',
+  newBandName: "",
 
-  isAddButtonDisabled: empty('newBandName'),
+  isAddButtonDisabled: empty("newBandName"),
 
   actions: {
     addBand() {
@@ -14,14 +14,18 @@ export default Controller.extend({
     },
 
     saveBand(event) {
-        event.preventDefault();
-        let newBand = Band.create({name: this.newBandName});
-        this.model.pushObject(newBand);
-        this.set('newBandName', '');
+      event.preventDefault();
+      let newBand = Band.create({ name: this.newBandName });
+      this.model.pushObject(newBand);
+      this.setProperties({
+        newBandName: "",
+        isAddingBand: false
+      });
+      this.transitionToRoute("bands.band.songs", newBand);
     },
 
     cancelAddBand() {
-        this.set("isAddingBand", false);
+      this.set("isAddingBand", false);
     }
   }
 });
--- tests/unit/controllers/songs-test.js
+++ tests/unit/controllers/songs-test.js
@@ -1,12 +1,12 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Controller | bands', function(hooks) {
+module('Unit | Controller | songs', function(hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
   test('it exists', function(assert) {
-    let controller = this.owner.lookup('controller:bands');
+    let controller = this.owner.lookup('controller:songs');
     assert.ok(controller);
   });
 });
