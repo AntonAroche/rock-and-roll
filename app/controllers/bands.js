166f6bce4198e36f67470110bcccc6d72131fd63
AntonAroche <anton.aroche@gmail.com>
2020-01-08 09:53:13 -0500

    Changed band adding option to a form so the user can submit by pressing Enter

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -1,16 +1,20 @@
 import Controller from "@ember/controller";
 import Band from 'rarwe/models/band';
+import { empty } from '@ember/object/computed';
 
 export default Controller.extend({
   isAddingBand: false,
   newBandName: '',
+
+  isAddButtonDisabled: empty('newBandName'),
 
   actions: {
     addBand() {
       this.set("isAddingBand", true);
     },
 
-    saveBand() {
+    saveBand(event) {
+        event.preventDefault();
         let newBand = Band.create({name: this.newBandName});
         this.model.pushObject(newBand);
         this.set('newBandName', '');
